5d394189c321c806a94be44b49ff8ed899494376
eru2 <steve020264@gmail.com>
2024-07-01 10:22:07 +0900

    Dev JAVA Web Project -[]

---
+++ a.bat
@@ -0,0 +1,17 @@
+@echo off
+echo=======================================
+echo [PROCESSING...] git pull
+gut pull
+echo [PROCESSING...} git add --all .
+git add --all .
+if [%1] EQU [] (
+echo {PROCESSING..] git commit -m "Dev JAVA Web Project %data%-[%time:~5,0%]"
+git commit -m "Dev JAVA Web Project %data%-[%time:~5,0%]"
+) else (
+echo [PROCESSING...] git commit -m %1
+git commit -m %1
+)
+echo [PROCESSING...] git push
+git push
+echo=========================================
+echo .
